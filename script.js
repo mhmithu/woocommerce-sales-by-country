3157185e8078785f75f1c353b93fb74c98e70026
Laurence Cope <info@amitywebsolutions.co.uk>
2018-06-27 17:41:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-jQuery(".wsc_datepicker").datepicker({
-    changeMonth: true,
-    changeYear: true,
-    defaultDate: "",
-    dateFormat: "yy-mm-dd",
-    numberOfMonths: 1,
-    maxDate: "+0D",
-    showButtonPanel: true,
-    showOn: "focus",
-    buttonImageOnly: true,
-    onSelect: function( selectedDate ) {
-        var option = jQuery(this).is('.from') ? "minDate" : "maxDate",
-            instance = jQuery(this).data("datepicker"),
-            date = jQuery.datepicker.parseDate(
-                instance.settings.dateFormat ||
-                jQuery.datepicker._defaults.dateFormat,
-                selectedDate, instance.settings);
-        dates.not(this).datepicker("option", option, date);
-    }
-});
+if( jQuery(".wsc_datepicker").length > 0 )
+{
+	jQuery(".wsc_datepicker").datepicker({
+	    changeMonth: true,
+	    changeYear: true,
+	    defaultDate: "",
+	    dateFormat: "yy-mm-dd",
+	    numberOfMonths: 1,
+	    maxDate: "+0D",
+	    showButtonPanel: true,
+	    showOn: "focus",
+	    buttonImageOnly: true,
+	    onSelect: function( selectedDate ) {
+	        var option = jQuery(this).is('.from') ? "minDate" : "maxDate",
+	            instance = jQuery(this).data("datepicker"),
+	            date = jQuery.datepicker.parseDate(
+	                instance.settings.dateFormat ||
+	                jQuery.datepicker._defaults.dateFormat,
+	                selectedDate, instance.settings);
+	        dates.not(this).datepicker("option", option, date);
+	    }
+	});
+}
